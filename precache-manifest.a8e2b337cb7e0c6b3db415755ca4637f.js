b20ccc9248b60ecd2435f53a7c081cd18ee53b62
jmwakz99 <kemsjunior99@gmail.com>
2020-01-14 14:04:53 +0300

    Updates

---
+++ precache-manifest.a8e2b337cb7e0c6b3db415755ca4637f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1e03b9eb2b68f1b451579f6776a79b",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "a5f2ee73b9e0db52f6e4",
+    "url": "/news/static/css/main.33b072e6.chunk.css"
+  },
+  {
+    "revision": "5d5a6a3f807a5f484a54",
+    "url": "/news/static/js/2.debc3057.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/news/static/js/2.debc3057.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5f2ee73b9e0db52f6e4",
+    "url": "/news/static/js/main.f39a0758.chunk.js"
+  },
+  {
+    "revision": "f0aa43568f6a15b7848f",
+    "url": "/news/static/js/runtime-main.85897639.js"
+  }
+]);
